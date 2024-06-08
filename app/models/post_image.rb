de330c53bc04c2903b5289a0c0faec2a22e23657
EC2 Default User <ec2-user@ip-172-31-38-3.ap-northeast-1.compute.internal>
2024-06-08 12:43:29 +0000

    [Finish] 21章【バリデーション・権限を設定しよう】

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,8 @@ class PostImage < ApplicationRecord
   belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  validates :shop_name, presence: true
+  validates :image, presence: true
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
