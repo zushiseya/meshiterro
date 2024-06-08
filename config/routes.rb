9ef45df8d752e1ae0a3375cde54f46e04787468d
EC2 Default User <ec2-user@ip-172-31-38-3.ap-northeast-1.compute.internal>
2024-06-08 07:57:21 +0000

    [Finish] 16章【ユーザーに関する機能(3) - ユーザー情報の編集】

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   get '/about', to: 'homes#about', as: 'about' 
   end
