c249a3ab38642ec91cea679a06aa7d0f3bc00f47
EC2 Default User <ec2-user@ip-172-31-38-3.ap-northeast-1.compute.internal>
2024-06-30 16:06:55 +0000

    [Finish] 管理者機能を実装しよう - 構成の改善（2）

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
-  root 'homes#top'
-  devise_for :users
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: :about
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
+  end
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
-  get '/about', to: 'homes#about', as: 'about' 
   end
